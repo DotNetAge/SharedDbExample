dc7177ef69856e884dff9fcc7ca6d47ff70d7010
Ray <csharp2002@hotmail.com>
2014-01-21 19:06:06 +0800

    Add complete CURD examples.

--- src/Controllers/TestController.cs
+++ src/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using DNA.Web;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SharedContextTest.Controllers
@@ -11,6 +12,83 @@ namespace SharedContextTest.Controllers
             var ps = ctx.Products.ToList();
             ViewBag.Products = ps;
             return View();
+        }
+
+        [SiteDashboard(Text = "Products", Group = "Store")]
+        public ActionResult List()
+        {
+            var ctx = new SharedDbContext();
+            var ps = ctx.Products.ToList();
+            ViewBag.Products = ps;
+            return View();
+        }
+
+        [SiteDashboard]
+        public ActionResult Detail(int id)
+        {
+            var ctx = new SharedDbContext();
+            var ps = ctx.Products.FirstOrDefault(c => c.ID == id);
+            //ViewBag.Products = ps;
+            return View(ps);
+        }
+
+
+        [SiteDashboard]
+        public ActionResult New() { return View(); }
+
+
+        [SiteDashboard]
+        public ActionResult Edit(int id)
+        {
+            var ctx = new SharedDbContext();
+            var ps = ctx.Products.Find(id);
+            return View(ps);
+        }
+
+
+        [HttpPost, SiteDashboard, ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, FormCollection foms)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var ctx = new SharedDbContext();
+                var ps = ctx.Products.Find(id);
+
+                if (this.TryUpdateModel(ps))
+                {
+
+                    ctx.SaveChanges();
+                }
+                return RedirectToAction("Detail", new { id = id });
+            }
+            return RedirectToAction("Edit", new { id = id });
+        }
+
+
+        [HttpPost, SiteDashboard, ValidateAntiForgeryToken]
+        public ActionResult New(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var ctx = new SharedDbContext();
+                ctx.Products.Add(product);
+                ctx.SaveChanges();
+                return RedirectToAction("Detail", new { id = product.ID });
+            }
+            return View();
+        }
+
+        [SiteDashboard, HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var ctx = new SharedDbContext();
+
+            var ps = ctx.Products.FirstOrDefault(c => c.ID == id);
+
+            ctx.Products.Remove(ps);
+            ctx.SaveChanges();
+            return RedirectToAction("List");
         }
     }
 }
